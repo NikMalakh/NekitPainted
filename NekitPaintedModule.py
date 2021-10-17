45e0ee537fb02e0ca61f87a31aa3afe5e99219d9
AppGrade Mikit <89784990+NikMalakh@users.noreply.github.com>
2021-10-17 09:32:14 +0300

    Update NekitPaintedModule.py

--- NekitPaintedModule.py
+++ NekitPaintedModule.py
@@ -2,9 +2,10 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from time import sleep
 import random  
+app = Client("my_account")
 chid=0
-@Client.on_message(filters.command("type", prefixes = "%")&filters.me)
-def type(_, msg):t
+@app.on_message(filters.command("type", prefixes = "/")&filters.me)
+def type(_, msg):
 	orig_text=msg.text.split("/type ", maxsplit = 1)[1]
 	text = orig_text
 	tbp = ""
@@ -19,7 +20,7 @@ def type(_, msg):t
 			sleep(0.05)
 		except FloodWait as e:
 			sleep(e.x) 
-@Client.on_message(filters.command("antifem", prefixes = "%")&filters.me)
+@app.on_message(filters.command("antifem", prefixes = "/")&filters.me)
 def ugnet(_, msg):
 	msg.edit("🤬 Начинаем угнетать фемок")
 	percent = 0
@@ -31,14 +32,77 @@ def ugnet(_, msg):
 		except FloodWait as e:
 			sleep(e.x)
 	msg.edit("✔️ Все фемки в чате успешно угнетены")
-@Client.on_message(filters.command("status", prefixes = "%")&filters.me)
+@app.on_message(filters.command("spam", prefixes = "/")&filters.me)
+def raid(_, msg):
+	orig_text=msg.text.split("/spam ", maxsplit = 1)[1]
+	text = orig_text
+	chid=msg.chat.id
+	msg.delete()
+	for i in range(1,50):
+		app.send_message(chid,text)
+		sleep(0.03)
+@app.on_message(filters.command("spamphoto", prefixes = "/")&filters.me)
+def photo(_, msg):
+	orig_text=msg.text.split("/spamphoto ", maxsplit = 1)[1]
+	text = orig_text
+	chid=msg.chat.id
+	msg.delete()
+	for i in range(1,25):
+		app.send_photo(chid,text)
+		sleep(0.05)
+@app.on_message(filters.command("spamvideo", prefixes = "/")&filters.me)
+def vid(_, msg):
+	orig_text=msg.text.split("/spamvideo ", maxsplit = 1)[1]
+	text = orig_text
+	chid=msg.chat.id
+	msg.delete()
+	for i in range(1,25):
+		app.send_video(chid,text)
+		sleep(0.075)
+@app.on_message(filters.command("spamgif", prefixes = "/")&filters.me)
+def gif(_, msg):
+	orig_text=msg.text.split("/spamgif ", maxsplit = 1)[1]
+	text = orig_text
+	chid=msg.chat.id
+	msg.delete()
+	for i in range(1,25):
+		app.send_animation(chid,text)
+		sleep(0.05)
+@app.on_message(filters.command("spamvoice", prefixes = "/")&filters.me)
+def aud(_, msg):
+	orig_text=msg.text.split("/spamvoice ", maxsplit = 1)[1]
+	text = orig_text
+	chid=msg.chat.id
+	msg.delete()
+	for i in range(1,25):
+		app.send_audio(chid,text)
+		sleep(0.05) 
+@app.on_message(filters.command("spamdoc", prefixes = "/")&filters.me)
+def doc(_, msg):
+	orig_text=msg.text.split("/spamdoc ", maxsplit = 1)[1]
+	text = orig_text
+	chid=msg.chat.id
+	msg.delete()
+	for i in range(1,25):
+		app.send_document(chid,text)
+		sleep(0.05) 
+@app.on_message(filters.command("spamsticker", prefixes = "/")&filters.me)
+def stick(_, msg):
+	orig_text=msg.text.split("/spamsticker ", maxsplit = 1)[1]
+	text = orig_text
+	chid=msg.chat.id
+	msg.delete()
+	for i in range(1,25):
+		app.send_sticker(chid,text)
+		sleep(0.05) 
+@app.on_message(filters.command("status", prefixes = "/")&filters.me)
 def status(_, msg):
 	orig_text=msg.text.split("/status ", maxsplit = 1)[1]
 	text = orig_text
 	chid=msg.chat.id
 	app.send_chat_action(chid, text)
 	msg.delete()
-@Client.on_message(filters.command("last", prefixes = "%")&filters.me)
+@app.on_message(filters.command("last", prefixes = "/")&filters.me)
 def search(_, msg):
 	orig_text=msg.text.split("/last ", maxsplit = 1)[1]
 	text = int(orig_text)
@@ -47,7 +111,7 @@ def search(_, msg):
 	for message in app.search_global(filter="empty" , limit=text):
 		app.send_message(chid, message.text)
 		sleep(0.1)
-@Client.on_message(filters.command("search", prefixes = "%")&filters.me)
+@app.on_message(filters.command("search", prefixes = "/")&filters.me)
 def seek(_, msg):
 	orig_text=msg.text.split("/search ", maxsplit = 1)[1]
 	text = orig_text
@@ -56,15 +120,15 @@ def seek(_, msg):
 	for message in app.search_global(text , limit=25):
 		app.send_message(chid, message.text)
 		sleep(0.1)
-@Client.on_message(filters.command("countmsg", prefixes = "%")&filters.me)
+@app.on_message(filters.command("countmsg", prefixes = "/")&filters.me)
 def count(_, msg):
 	chid=msg.chat.id
 	msg.edit("<b>Количество сообщений в чате:</b> "+str(app.get_history_count(chid))) 
-@Client.on_message(filters.command("help", prefixes = "%")&filters.me)
+@app.on_message(filters.command("help", prefixes = "/")&filters.me)
 def count(_, msg):
 	chid=msg.chat.id
-	msg.edit("<b>Список команд юзербота</b>\n\n<b><i>Анимации: </i></b><code>/antifem</code>, <code>/type</code> <i>текст</i>, <code>/ticker</code> <i>текст</i>\n<b><i>Фейковый статус: </i></b><code>/status</code> <i>typing|upload_photo|upload_video|upload_audio|upload_document|find_location|upload_video_note|choose_contact|playing|speaking|cancel</i>\n<b><i>Поиск сообщений: </i></b><code>/last</code> <i>кол-во сообщений</i>, <code>/search</code> <i>запрос</i>\n<b><i>Информация о чате: </i></b><code>/countmsg</code>") 
-@app.on_message(filters.command("ticker", prefixes = "%")&filters.me)
+	msg.edit("<b>Список команд юзербота</b>\n\n<b><i>Спам: </i></b><code>/spam</code> <i>текст</i>, <code>/spamphoto</code> <i>фото</i>, <code>/spamvideo</code> <i>видео</i>, <code>/spamsticker</code> <i>стикер</i>, <code>/spamdoc</code> <i>файл</i>, <code>/spamgif</code> <i>анимация</i>, <code>/spamvoice</code> <i>голосовое сообщение</i>\n<b><i>Анимации: </i></b><code>/antifem</code>, <code>/type</code> <i>текст</i>, <code>/ticker</code> <i>текст</i>\n<b><i>Фейковый статус: </i></b><code>/status</code> <i>typing|upload_photo|upload_video|upload_audio|upload_document|find_location|upload_video_note|choose_contact|playing|speaking|cancel</i>\n<b><i>Поиск сообщений: </i></b><code>/last</code> <i>кол-во сообщений</i>, <code>/search</code> <i>запрос</i>\n<b><i>Информация о чате: </i></b><code>/countmsg</code>") 
+@app.on_message(filters.command("ticker", prefixes = "/")&filters.me)
 def tck(_, msg):
 	orig_text=msg.text.split("/ticker ", maxsplit = 1)[1]
 	text = orig_text
