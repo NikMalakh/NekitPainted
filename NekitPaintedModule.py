ab53f16de5dc4abc698d612c6c52dde4fb750978
AppGrade Mikit <89784990+NikMalakh@users.noreply.github.com>
2021-11-20 13:45:25 +0300

    Update NekitPaintedModule.py
    Fixed error with message search

--- NekitPaintedModule.py
+++ NekitPaintedModule.py
@@ -111,8 +111,9 @@ def search(_, msg):
 	chid=msg.chat.id
 	msg.delete()
 	for message in _.search_global(filter="empty" , limit=text):
-		_.send_message(chid, "<i>"+message.from_user.first_name+"</i><b> in </b><i>"+message.chat.title+": </i>"+message.text)
-		sleep(0.1)
+		if message.chat.type=="group" or message.chat.type=="supergroup":
+			_.send_message(chid, "<i>"+message.from_user.first_name+"</i><b> in </b><i>"+message.chat.title+": </i>"+message.text)
+			sleep(0.1)
 @Client.on_message(filters.command("nmsearch", prefixes = "%")&filters.me)
 def seek(_, msg):
 	count = int(msg.text.split(" ")[1]) 
@@ -121,8 +122,9 @@ def seek(_, msg):
 	chid=msg.chat.id
 	msg.delete()
 	for message in _.search_global(text , limit=count):
-		_.send_message(chid, "<i>"+message.from_user.first_name+"</i><b> in </b><i>"+message.chat.title+": </i>"+message.text)
-		sleep(0.1)
+		if message.chat.type=="group" or message.chat.type=="supergroup":
+			_.send_message(chid, "<i>"+message.from_user.first_name+"</i><b> in </b><i>"+message.chat.title+": </i>"+message.text)
+			sleep(0.1)
 @Client.on_message(filters.command("nmctype", prefixes = "%")&filters.me)
 def ctype(_, msg):
 	symbol=msg.text.split(" ")[1]
@@ -167,4 +169,4 @@ def tester(_, msg):
 	msg.edit("<i><b>Module works good</b></i>")
 @Client.on_message(filters.command("nmversion", prefixes = "%")&filters.me)
 def version(_, msg):
-	msg.edit("<i><b>Nekit Painted Module</b> for <b>Painted-Userbot</b> v1.0.1\nDo not distribute</i>")
+	msg.edit("<i><b>Nekit Painted Module</b> for <b>Painted-Userbot</b> v1.0.2\nDo not distribute</i>")
