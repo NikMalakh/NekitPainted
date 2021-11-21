ea27804b3566a95d800ea8a693efbbe3d81309bd
AppGrade Mikit <89784990+NikMalakh@users.noreply.github.com>
2021-11-21 13:53:08 +0300

    Update NekitPaintedModule.py
    fix

--- NekitPaintedModule.py
+++ NekitPaintedModule.py
@@ -37,7 +37,7 @@ def ugnet(_, msg):
 @Client.on_message(filters.command("nmspam", prefixes = "%")&filters.me)
 def raid(_, msg):
 	orig_text=msg.text.split(" ", maxsplit=2)[2]
-	count=msg.text.split(" ")[1]
+	count=int(msg.text.split(" ")[1])
 	text = orig_text
 	chid=msg.chat.id
 	msg.delete()
@@ -47,7 +47,7 @@ def raid(_, msg):
 @Client.on_message(filters.command("nmspamphoto", prefixes = "%")&filters.me)
 def photo(_, msg):
 	orig_text=msg.text.split(" ", maxsplit=2)[2]
-	count=msg.text.split(" ")[1]
+	count=int(msg.text.split(" ")[1])
 	text = orig_text
 	chid=msg.chat.id
 	msg.delete()
@@ -57,7 +57,7 @@ def photo(_, msg):
 @Client.on_message(filters.command("nmspamvideo", prefixes = "%")&filters.me)
 def vid(_, msg):
 	orig_text=msg.text.split(" ", maxsplit=2)[2]
-	count=msg.text.split(" ")[1]
+	count=int(msg.text.split(" ")[1])
 	text = orig_text
 	chid=msg.chat.id
 	msg.delete()
@@ -67,7 +67,7 @@ def vid(_, msg):
 @Client.on_message(filters.command("nmspamgif", prefixes = "%")&filters.me)
 def gif(_, msg):
 	orig_text=msg.text.split(" ", maxsplit=2)[2]
-	count=msg.text.split(" ")[1]
+	count=int(msg.text.split(" ")[1])
 	text = orig_text
 	chid=msg.chat.id
 	msg.delete()
@@ -77,7 +77,7 @@ def gif(_, msg):
 @Client.on_message(filters.command("nmspamvoice", prefixes = "%")&filters.me)
 def aud(_, msg):
 	orig_text=msg.text.split(" ", maxsplit=2)[2]
-	count=msg.text.split(" ")[1]
+	count=int(msg.text.split(" ")[1])
 	text = orig_text
 	chid=msg.chat.id
 	msg.delete()
@@ -87,7 +87,7 @@ def aud(_, msg):
 @Client.on_message(filters.command("nmspamdoc", prefixes = "%")&filters.me)
 def doc(_, msg):
 	orig_text=msg.text.split(" ", maxsplit=2)[2]
-	count=msg.text.split(" ")[1]
+	count=int(msg.text.split(" ")[1])
 	text = orig_text
 	chid=msg.chat.id
 	msg.delete()
@@ -97,7 +97,7 @@ def doc(_, msg):
 @Client.on_message(filters.command("nmspamsticker", prefixes = "%")&filters.me)
 def stick(_, msg):
 	orig_text=msg.text.split(" ", maxsplit=2)[2]
-	count=msg.text.split(" ")[1]
+	count=int(msg.text.split(" ")[1])
 	text = orig_text
 	chid=msg.chat.id
 	msg.delete()
@@ -160,7 +160,7 @@ def count(_, msg):
 @Client.on_message(filters.command("nmhelp", prefixes = "%")&filters.me)
 def help(_, msg):
 	chid=msg.chat.id
-	msg.edit("<b>Nekit Painted Module commands:</b>\n\n<b><i>Message flood: </i></b><code>%nmspam</code> <i>text</i>, <code>%nmspamphoto</code> <i>photo URL</i>, <code>%nmspamvideo</code> <i>video URL</i>, <code>%nmspamsticker</code> <i>sticker URL</i>, <code>%nmspamdoc</code> <i>document URL</i>, <code>%nmspamgif</code> <i>GIF URL</i>, <code>%nmspamvoice</code> <i>audio URL</i>\n<b><i>Animations: </i></b><code>%nmantifem</code>, <code>%nmtype</code> <i>text</i>, <code>%nmctype</code> <i>[symbol] [text]</i>, <code>%nmticker</code> <i>text</i>\n<b><i>Chat action simulation: </i></b><code>%nmstatus</code> <i>typing|upload_photo|upload_video|upload_audio|upload_document|find_location|upload_video_note|choose_contact|playing|speaking|cancel</i>\n<b><i>Message search: </i></b><code>%nmlast</code> <i>message limit</i>, <code>%nmsearch</code> <i>[message limit] [query]</i>\n<b><i>Chat information: </i></b><code>%nmcountmsg</code>\n<b><i>Calculations: </i></b> <code>%nmrand</code> <i>[lower limit] [higher limit]</i>, <code>%nmcalc</code> <i>expression <b><u>(unsafe!)</u></b></i>\n<b><i>Technical commands: </i></b> <code>%nnmtest</code>, <code>%nmversion</code>") 
+	msg.edit("<b>Nekit Painted Module commands:</b>\n\n<b><i>Message flood: </i></b><code>%nmspam</code> <i>[amount] [text]</i>, <code>%nmspamphoto</code> <i>[amount] [URL]</i>, <code>%nmspamvideo</code> <i>[amount] [URL]</i>, <code>%nmspamsticker</code> <i>[amount] [URL]</i>, <code>%nmspamdoc</code> <i>[amount] [URL]</i>, <code>%nmspamgif</code> <i>[amount] [URL]</i>, <code>%nmspamvoice</code> <i>[amount] [URL]</i>\n<b><i>Animations: </i></b><code>%nmantifem</code>, <code>%nmtype</code> <i>text</i>, <code>%nmctype</code> <i>[symbol] [text]</i>, <code>%nmticker</code> <i>text</i>\n<b><i>Chat action simulation: </i></b><code>%nmstatus</code> <i>typing|upload_photo|upload_video|upload_audio|upload_document|find_location|upload_video_note|choose_contact|playing|speaking|cancel</i>\n<b><i>Message search: </i></b><code>%nmlast</code> <i>message limit</i>, <code>%nmsearch</code> <i>[message limit] [query]</i>\n<b><i>Chat information: </i></b><code>%nmcountmsg</code>\n<b><i>Calculations: </i></b> <code>%nmrand</code> <i>[lower limit] [higher limit]</i>, <code>%nmcalc</code> <i>expression <b><u>(unsafe!)</u></b></i>\n<b><i>Technical commands: </i></b> <code>%nnmtest</code>, <code>%nmversion</code>") 
 @Client.on_message(filters.command("nmticker", prefixes = "%")&filters.me)
 def tcker(_, msg):
 	orig_text=msg.text.split("%nmticker ", maxsplit = 1)[1]
